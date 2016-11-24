83f1220ef737022f18521d3d98fb88d6fbb31a86
D3adKnight <jack.super.hacker@gmail.com>
2016-11-25 02:38:32 +0300

    I want game =)

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/assets"));
+//app.use(express.static(__dirname + "/views"));
 
-app.set('port', (process.env.PORT | 3000));
+app.set("port", (process.env.PORT | 3000));
+app.set("view engine", 'ejs');
+app.set("views", __dirname + "/views");
 
 app.get('/', function(req, res){
-	res.sendFile("index.html");
+	res.render("pages/index.ejs");
 });
 
 server.listen(3000);
--- public/js/src/game.js
+++ public/js/src/game.js
@@ -1,91 +1,163 @@
-/*
-    Init globals
-*/
-let canvas = makeCanvas(512, 512);
-stage.width = canvas.width;
-stage.height = canvas.height;
+const KEY_CODES = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
 
-let fps = 30,
-    previous = 0,
-    lag = 0,
-    frameDuration = 1000 / fps;
+var KEY_STATUS = {};
+for(var code in KEY_CODES) {
+    KEY_STATUS[KEY_CODES[code]] = false;
+}
 
-let ship;
-let hor_speed = 5;
+var bg = new Image();
+bg.addEventListener("load", start, false);
+bg.src = "/bgs/black.png";
 
-assets.load(["/assets/sprites/sheet.json"])
-.then(() => setup());
+var ship_img = new Image();
+ship_img.src = "/images/playerShip1_red.png";
 
-function setup() {
-    ship = sprite(assets["playerShip1_red.png"], 256, 256);
-    
-    stage.putCenter(ship);
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext('2d');
 
-    ship.vx = 0;
-    ship.vy = 0;
-    ship.accelerationX = 0.2;
-    ship.accelerationY = 0.2;
-    ship.frictionX = 0.96;
-    ship.frictionY = 0.96;
-
-    ship.rotationSpeed = 0;
-
-    ship.moveForward = false;
-
-    let leftArrow = keyboard(37),
-        rightArrow = keyboard(39),
-        upArrow = keyboard(38);
-
-    leftArrow.press = () => ship.rotationSpeed = -0.1;
-    leftArrow.release = () => {
-        if(!rightArrow.isDown) ship.rotationSpeed = 0;
-    };
-    rightArrow.press = () => ship.rotationSpeed = 0.1;
-    rightArrow.release = () => {
-        if(!leftArrow.isDown) ship.rotationSpeed = 0;
-    };
-
-    upArrow.press = () => ship.moveForward = true;
-    upArrow.release = () => ship.moveForward = false;
-
+function start() {
+    setup();
     gameLoop();
 }
 
-function update() {
-    ship.rotation += ship.rotationSpeed;
+var ship;
 
-    if(ship.moveForward) {
-        ship.vx += ship.accelerationX * Math.cos(ship.rotation);
-        ship.vy += ship.accelerationY * Math.sin(ship.rotation);
-    } else {
-        ship.vx *= ship.frictionX;
-        ship.vy *= ship.frictionY;
-    }
+var bg_offset_x = 0;
+var bg_offset_y = 0;
+var ship_w = ship_img.width / 2;
+var ship_h = ship_img.height / 2;
 
-    ship.x += ship.vx;
-    ship.y += ship.vy;
+var ship_x = 0;
+var ship_y = 0;
+var ship_vx = 0;
+var ship_vy = 0;
+var ship_rotation = 0;
+var ship_acceleration = 0.2;
+var ship_friction = 0.96;
+var ship_moveForward = false;
 
-    contain(ship, stage.localBounds);
+function setup() {
+    ship_x = (canvas.width - ship_w) / 2;
+    ship_y = (canvas.height - ship_h) / 2;
+
+    ship = new Sprite(ship_img, ship_x, ship_y, ship_w, ship_h);
+
+    document.onkeydown = function(e) {
+        var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+        if(KEY_CODES[keyCode]) {
+            e.preventDefault();
+            KEY_STATUS[KEY_CODES[keyCode]] = true;
+        }
+    };
+
+    document.onkeyup = function(e) {
+        var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+        if(KEY_CODES[keyCode]) {
+            e.preventDefault();
+            KEY_STATUS[KEY_CODES[keyCode]] = false;
+        }
+    };
 }
 
-function gameLoop(timestamp) {
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw background
+    var pattern = ctx.createPattern(bg, "repeat");
+    ctx.fillStyle = pattern;
+
+    ctx.translate(bg_offset_x, bg_offset_y);
+    ctx.fillRect(-bg_offset_x, -bg_offset_y, canvas.width, canvas.height);
+    ctx.translate(-bg_offset_x, -bg_offset_y);
+
+    // draw ship
+    ctx.save();
+    ctx.translate(
+        ship_x + ship_w * 0.5,
+        ship_y + ship_h * 0.5
+    );
+    ctx.rotate(ship_rotation);
+    /*
+    ctx.drawImage(ship, 0, 0,
+                 ship.width, ship.height,
+                 -ship_w * 0.5, -ship_h * 0.5,
+                 ship_w, ship_h);
+    */
+    ship.render(ctx);
+    ctx.restore();
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+        canvas.width * 0.5 - 2,
+        canvas.height * 0.5 - 2,
+        4, 4
+    );
+}
+
+function wrap() {
+    if(ship_x - ship_w * 2 < 0) {
+        ship_x = canvas.width - ship_w;
+    }
+    if(ship_y - ship_h < 0) {
+        ship_y = canvas.height - ship_h;
+    }
+    if(ship_x + ship_w * 2 > canvas.width) {
+        ship_x = 0;
+    }
+    if(ship_y + ship_h > canvas.height) {
+        ship_y = 0;
+    }
+}
+
+function gameLoop() {
     requestAnimationFrame(gameLoop);
 
-    if(!timestamp) timestamp = 0;
-    let elapsed = timestamp - previous;
-
-    if(elapsed > 1000) elapsed = frameDuration;
-    lag += elapsed;
-
-    while (lag >= frameDuration) {
-        capturePreviousPositions(stage);
-        update();
-
-        lag -= frameDuration;
+    // update ship position
+    if(KEY_STATUS.left) {
+        ship_rotation -= 0.1;
+    }
+    if(KEY_STATUS.right) {
+        ship_rotation += 0.1;
+    }
+    if(KEY_STATUS.up) {
+        ship_moveForward = true;
+    }
+    if(ship_moveForward && !KEY_STATUS.up) {
+        ship_moveForward = false;
+    }
+    
+    if(ship_moveForward) {
+        ship_vx += ship_acceleration * Math.sin(ship_rotation);
+        ship_vy += -ship_acceleration * Math.cos(ship_rotation);
+    } else {
+        ship_vx *= ship_friction;
+        ship_vy *= ship_friction;
     }
 
-    let lagOffset = lag / frameDuration;
-    renderWithInterpolation(canvas, lagOffset);
+    ship_x += ship_vx;
+    ship_y += ship_vy;
 
-    previous = timestamp;
+    bg_offset_x += ship_vx * 0.2;
+    bg_offset_y += ship_vy * 0.2;
+
+    wrap();
+
+    // rendering
+    render();
 }
+
+/*
+ctx.drawImage(
+ this.source,
+ this.sourceX, this.sourceY,
+ this.sourceWidth, this.sourceHeight,
+ -this.width * this.pivotX,
+ -this.height * this.pivotY,
+ this.width, this.height
+ );
+*/
